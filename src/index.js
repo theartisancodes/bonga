4c158560692ee72a387e578d2cfc8a38764ef415
nzaujk <joseph.nzau@andela.com>
2019-03-16 15:09:13 +0300

    implement login and register, setup  firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './components/App';
+import * as serviceWorker from './utils/serviceWorker';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+
 serviceWorker.unregister();
